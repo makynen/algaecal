b4e3952163d16b8353915ae953bdf304840e482f
makynen <42380193+makynen@users.noreply.github.com>
2018-08-15 21:15:17 -0400

    Initial commit

---
+++ src/js/ac-functions.js
@@ -0,0 +1,44 @@
+const acphone = '1-800-820-0184'; 
+
+document.getElementById("ac-call").innerHTML = `
+<p class="ac-tap-to-talk">Tap to Talk <span class="ac-phone">${acphone}</span></p>
+<p><i class="fa fa-phone" aria-hidden="true"></i> Speak to Our Bone Health Specialists</p>
+`
+
+/* Hide Packages + Wistia Fallback */
+$(document).ready(function() {
+    $("#ac-packages").hide(); 
+document.getElementById('cecdwaq3dz').addEventListener("timeupdate", 
+function() {
+    // Show packages if time is > 2:13...
+    if(this.currentTime > 133) {
+$("#ac-packages").show();
+    }
+    else {
+        // Unlike the Wistia script we'll re-hide the packages if time returns to < 2:13...
+        $("#ac-packages").hide();
+    }
+});
+});
+
+/* Back to Top */
+$(document).ready(function(){
+    $(window).scroll(function () {
+           if ($(this).scrollTop() > 50) {
+               $('#back-to-top').fadeIn();
+           } else {
+               $('#back-to-top').fadeOut();
+           }
+       });
+       // Scroll to 0px on click...
+       $('#back-to-top').click(function () {
+           $('#back-to-top').tooltip('hide');
+           $('body,html').animate({
+               scrollTop: 0
+           }, 800);
+           return false;
+       });
+       
+       $('#back-to-top').tooltip('show');
+
+});
